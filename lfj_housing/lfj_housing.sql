a6048900e0638487e99a2cbaabea4a767837e775
LeFruJohn0 <87871872+LeFruJohn0@users.noreply.github.com>
2021-08-03 13:51:56 +0300

    Update lfj_housing.sql

--- lfj_housing/lfj_housing.sql
+++ lfj_housing/lfj_housing.sql
@@ -1,1 +1,19 @@
 ALTER TABLE vrp_users ADD inhouse int NOT NULL DEFAULT 0;
+
+CREATE TABLE `lfj_housing` (
+  `id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `sellingprice` int(11) NOT NULL,
+  `type` text NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `x` varchar(255) NOT NULL,
+  `y` varchar(255) NOT NULL,
+  `z` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE `lfj_housing`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `lfj_housing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
